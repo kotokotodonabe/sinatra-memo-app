f06cac95472ad42fc279bcae05b66ff554c7a7fc
tosan777 <tosan.golang1.14.on@gmail.com>
2022-08-08 14:43:16 +0900

    メモを追加して表示させる機能までを作成
    【機能】
    トップページに追加したメモを表示
    メモを保存した際に、TOPページにGETリクエストでアクセスする

--- top.rb
+++ top.rb
@@ -1,7 +1,19 @@
 require "sinatra"
 require "sinatra/reloader"
+require "csv"
 
 get "/" do
+
+  arr = []
+  CSV.foreach("post.csv") do |line|
+    arr << line
+  end
+
+  @title = []
+  arr.each do |frame|
+    @title << frame[0]
+  end
+
   erb :index
 end
 
@@ -15,4 +27,14 @@ end
 
 get "/edit" do
   erb :edit
+end
+
+post "/" do
+  @title = params[:title]
+  @text = params[:text]
+  CSV.open("post.csv", "a") do |csv|
+    csv << [@title, @text]
+  end
+
+  redirect to('/')
 end
